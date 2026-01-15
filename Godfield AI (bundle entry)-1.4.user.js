aaf585686ee2fe1583b5d9c3f78ce86171515fbe
naoyaAAAAA <hasegawainside@gmail.com>
2026-01-16 00:59:36 +0900

    Add files via upload

---
+++ Godfield AI (bundle entry)-1.4.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Godfield AI (bundle entry)
+// @namespace    http://tampermonkey.net/
+// @version      1.4
+// @description  Bundle loader for Godfield AI
+// @match        https://godfield.net/*
+// @match        https://www.godfield.net/*
+// @grant        GM_xmlhttpRequest
+// @grant        unsafeWindow
+// @grant        GM_download
+// @run-at       document-end
+// @connect      127.0.0.1
+// @connect      localhost
+// @connect      *
+// @require      file:///C:/godfieldAI/dist/bundle.user.js
+// ==/UserScript==
+
